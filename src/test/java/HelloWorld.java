0f4805ede1ee2c6663af7b465042385c83415042
Begiasyl <asylbekovafleet@gmail.com>
2024-01-04 18:11:07 -0800

    Removed

--- src/test/java/HelloWorld.java
+++ src/test/java/HelloWorld.java
@@ -2,6 +2,5 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello Begimai!");
         System.out.println("This is my second change ");
-        System.out.println("hihi");
     }
 }
