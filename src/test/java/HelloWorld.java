ebefbbfb82b3fdf91ade199b6ca04f21246d9dc1
Begiasyl <asylbekovafleet@gmail.com>
2024-01-04 18:10:39 -0800

    another change

--- src/test/java/HelloWorld.java
+++ src/test/java/HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello Begimai!");
         System.out.println("This is my second change ");
+        System.out.println("hihi");
     }
 }
